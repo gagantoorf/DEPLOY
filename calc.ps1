62bc970efea7f939c0757df146ba6178a04bd0cf
gagantoorf <80358334+gagantoorf@users.noreply.github.com>
2021-05-03 18:27:09 -0400

    Update calc.ps1

--- calc.ps1
+++ calc.ps1
@@ -1,1 +1,1 @@
-Get-AppxPackage -allusers Microsoft.WindowsCalculator | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml”}
+Get-AppXPackage -AllUsers -Name Microsoft.WindowsCalculator | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml” -Verbose}
