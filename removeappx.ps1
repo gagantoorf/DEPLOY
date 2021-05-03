afcf6d2e1cabeb1ae0884bd1a74ac1159d56b5cf
gagantoorf <80358334+gagantoorf@users.noreply.github.com>
2021-05-03 18:15:50 -0400

    Update removeappx.ps1

--- removeappx.ps1
+++ removeappx.ps1
@@ -1,1 +1,2 @@
 Get-AppxPackage -AllUsers | Remove-AppxPackage
+Get-AppxPackage -allusers Microsoft.WindowsCalculator | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register “$($_.InstallLocation)\AppXManifest.xml”}
