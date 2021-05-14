2a835bf016756c3bd47b284342f2774367ef545c
gagantoorf <80358334+gagantoorf@users.noreply.github.com>
2021-05-14 10:09:27 -0400

    Update EnableBitlocker.ps1

--- EnableBitlocker.ps1
+++ EnableBitlocker.ps1
@@ -1,1 +1,1 @@
-Enable-Bitlocker -MountPoint c: -UsedSpaceOnly -SkipHardwareTest -RecoveryKeyPath "D:\Recovery\" -RecoveryKeyProtector
+Enable-Bitlocker -MountPoint c: -UsedSpaceOnly -SkipHardwareTest -RecoveryKeyPath "C:\Recovery\" -RecoveryKeyProtector
