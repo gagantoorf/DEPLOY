ee18b4c026ed3a37d96a7d3af4e3e2beb75a1f91
gagantoorf <80358334+gagantoorf@users.noreply.github.com>
2021-06-11 16:05:19 -0400

    Update Teams.bat

--- Teams.bat
+++ Teams.bat
@@ -1,4 +1,4 @@
 MKDIR C:\TMPTeams
-xcopy /E /Y "https://cgl-apps.s3.us-east-2.amazonaws.com/Teams_windows_x64.exe" c:\TMPTeams\Teams_windows_x64.exe
+Invoke-WebRequest https://cgl-apps.s3.us-east-2.amazonaws.com/Teams_windows_x64.exe c:\TMPTeams\Teams_windows_x64.exe
 
 
