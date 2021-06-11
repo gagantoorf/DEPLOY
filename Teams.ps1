b66cbb0b9dd435c77ede49e0acf4584aeec3989b
gagantoorf <80358334+gagantoorf@users.noreply.github.com>
2021-06-11 16:12:46 -0400

    Update Teams.ps1

--- Teams.ps1
+++ Teams.ps1
@@ -1,4 +1,3 @@
-MKDIR C:\TMPTeams
-Invoke-WebRequest https://cgl-apps.s3.us-east-2.amazonaws.com/Teams_windows_x64.exe c:\TMPTeams\Teams_windows_x64.exe
-
-
+powershell.exe -command "Invoke-WebRequest  https://cgl-apps.s3.us-east-2.amazonaws.com/Teams_windows_x64.exe-Outfile c:\TMPTeams\Teams_windows_x64.exe"
+ping -n 20 127.0.0.1 >NUL
+msiexec.exe /i c:\TMPTeams\Teams_windows_x64.exe
