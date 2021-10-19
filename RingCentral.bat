f93b21d66d7386a2cee510e11fb7ae7b6df318fb
gagantoorf <80358334+gagantoorf@users.noreply.github.com>
2021-10-19 17:46:25 -0400

    Update RingCentral.bat

--- RingCentral.bat
+++ RingCentral.bat
@@ -4,6 +4,6 @@ if exist "%USERPROFILE%\AppData\Local\RingCentral\" GOTO :END
 
 REM Modify the below UNC path to match where your files are located
 
-msiexec.exe /i \\gt02\Public\AppTEST\RingCentral-x64.msi /qn /l*v C:\windows\temp\rcdesktop_install.log
+msiexec.exe /i https://app.ringcentral.com/download/RingCentral-x64.msi /qn /l*v C:\windows\temp\rcdesktop_install.log
 
 :END
